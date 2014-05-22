07a02c438864b0f71bfb6385a82e96413fe73e32
mpfingsten <m.pfingsten@greylogix.de>
2014-05-22 11:23:35 +0200

    Content scales with window size.
    Added Icons to Tiles

--- bgx_caw_backend/bgx_caw_backend/MainWindow.xaml.cs
+++ bgx_caw_backend/bgx_caw_backend/MainWindow.xaml.cs
@@ -25,7 +25,7 @@ namespace bgx_caw_backend
     /// <summary>
     /// Interaktionslogik für MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : MetroWindow
+    public partial class MainWindow
     {
         private List<Diagramm> diagrammsList;
         private DataSet diagrammsSet;
@@ -36,9 +36,7 @@ namespace bgx_caw_backend
         public MainWindow()
         {
             InitializeComponent();
-            refreshDiagrammList();
-
-            flo_right.IsOpen = false;       
+            refreshDiagrammList();      
         }
 
         private void refreshDiagrammList()
@@ -48,8 +46,6 @@ namespace bgx_caw_backend
                 using (DB_CAW db_caw = new DB_CAW())
                 {
                     diagrammsList = db_caw.getDiagramms();
-                    //diagrammsSet = db_caw.getDiagrammsSet();
-
                     dgd_diagrammsList.ItemsSource = diagrammsList;
                     
                 }
@@ -97,7 +93,7 @@ namespace bgx_caw_backend
             //this.ShowMessageAsync("Hello!", "Welcome to the world of metro! ",
               //  MessageDialogStyle.AffirmativeAndNegativeAndSingleAuxiliary, mySettings);
 
-            flo_right.IsOpen = true;
+            //flo_right.IsOpen = true;
             
         }
 
@@ -108,8 +104,8 @@ namespace bgx_caw_backend
                 AffirmativeButtonText = "Cancel",
                 ColorScheme = MetroDialogColorScheme.Accented
             };
-            this.ShowMessageAsync("Bitte warten...", "Schaltplan wird importiert ",
-            MessageDialogStyle.Affirmative, mySettings);
+            //this.ShowMessageAsync("Bitte warten...", "Schaltplan wird importiert ",
+            //MessageDialogStyle.Affirmative, mySettings);
 
 
         }
@@ -130,7 +126,7 @@ namespace bgx_caw_backend
             if (folderDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
                 sourceFolder = new DirectoryInfo(folderDialog.SelectedPath);
-                tbox_sourceFolder.Text = folderDialog.SelectedPath;
+                //tbox_sourceFolder.Text = folderDialog.SelectedPath;
             }
         }
 
@@ -142,18 +138,89 @@ namespace bgx_caw_backend
             {
 
                 db_caw.addDiagramm(dxf_parser.Diagramm);
-                //pgb_load.IsIndeterminate = false;
-                flo_bottom.IsOpen = false;
-                this.ShowProgressAsync("Please wait...", "Progress message");
-
-                
-
-
-                //this.DialogResult = true;
             }
 
         }
 
-        
+        private void flo_bottom_tle_ImportFolder_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+
+            if (folderDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                sourceFolder = new DirectoryInfo(folderDialog.SelectedPath);
+                //tbox_sourceFolder.Text = folderDialog.SelectedPath;
+            }
+        }
+
+        private void ttl_mnu_Import_Click(object sender, RoutedEventArgs e)
+        {
+            flo_bottom.IsOpen = true;
+        }
+
+        private void FlipView_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        #region ScaleValue Depdency Property
+        public static readonly DependencyProperty ScaleValueProperty = DependencyProperty.Register("ScaleValue", typeof(double), typeof(MainWindow), new UIPropertyMetadata(1.0, new PropertyChangedCallback(OnScaleValueChanged), new CoerceValueCallback(OnCoerceScaleValue)));
+
+        private static object OnCoerceScaleValue(DependencyObject o, object value)
+        {
+            MainWindow mainWindow = o as MainWindow;
+            if (mainWindow != null)
+                return mainWindow.OnCoerceScaleValue((double)value);
+            else
+                return value;
+        }
+
+        private static void OnScaleValueChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
+        {
+            MainWindow mainWindow = o as MainWindow;
+            if (mainWindow != null)
+                mainWindow.OnScaleValueChanged((double)e.OldValue, (double)e.NewValue);
+        }
+
+        protected virtual double OnCoerceScaleValue(double value)
+        {
+            if (double.IsNaN(value))
+                return 1.0d;
+
+            value = Math.Max(0.1, value);
+            return value;
+        }
+
+        protected virtual void OnScaleValueChanged(double oldValue, double newValue)
+        {
+
+        }
+
+        public double ScaleValue
+        {
+            get
+            {
+                return (double)GetValue(ScaleValueProperty);
+            }
+            set
+            {
+                SetValue(ScaleValueProperty, value);
+            }
+        }
+        #endregion
+
+        private void StackPanel_SizeChanged_1(object sender, EventArgs e)
+        {
+            CalculateScale();
+        }
+
+        private void CalculateScale()
+        {
+            double yScale = ActualHeight / 750.0d;
+            double xScale = ActualWidth / 1200.0d;
+            double value = Math.Min(xScale, yScale);
+            ScaleValue = (double)OnCoerceScaleValue(stp_Main, value);
+        }
+ 
     }
 }
