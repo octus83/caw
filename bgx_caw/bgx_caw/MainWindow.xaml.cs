d93e08cf99738760d38571c34dd1abe8b1115df5
Hendrik <Schlecht_1987@hotmail.com>
2014-06-02 10:55:25 +0200

    -variablen namen angepasst - seiten flyout

--- bgx_caw/bgx_caw/MainWindow.xaml.cs
+++ bgx_caw/bgx_caw/MainWindow.xaml.cs
@@ -39,9 +39,7 @@ namespace bgx_caw
         }
         public MainWindow()
         {
-            InitializeComponent();
-
-            //Test
+            InitializeComponent();           
         }
 
         private void Button_open_project(object sender, RoutedEventArgs e)
@@ -52,60 +50,115 @@ namespace bgx_caw
 
         private void Button_next_page(object sender, RoutedEventArgs e)
         {
+            nextPage();
+        }
+
+        private void Button_previous_page(object sender, RoutedEventArgs e)
+        {
+            previousPage();
+        }
+
+        private void Button_open_info(object sender, RoutedEventArgs e)
+        {
+            flo_right_info.IsOpen = true;
+        }
+
+        private void Tile_Potentiale(object sender, RoutedEventArgs e)
+        {
+            buildPotentialFlyout(); 
+        }
+        private void Tile_Potential_Click(object sender, EventArgs e)
+        {
+            stack_right_sites.Children.Clear();
+            Tile tile = sender as Tile;
+            DB_CAW t = new DB_CAW();
+            List<String> list = new List<String>();
+           list= t.makeQuery(Query.getPotentialPagenumbersFromPotentialNames(tile.Title, this.ID));
+           flo_right_sites.IsOpen = true;
+           foreach (var item in list)
+           {
+               Tile t1 = new Tile();
+               t1.Title = item;
+               t1.TiltFactor = 2;
+               t1.Width = 150;
+               t1.Height = 50;
+               tile.Click += new RoutedEventHandler(Tile_Site_Click);
+               stack_right_sites.Children.Add(t1);
+               
+
+           }
+        }
+
+        private void Tile_Site_Click(object sender, EventArgs e)
+        {
+            Tile tile = sender as Tile;
+            try
+            {
+              goToPage(Convert.ToInt32(tile.Title)); 
+            }
+            catch (Exception)
+            {               
+                throw;
+            }
+        }
+
+        private void buildPotentialFlyout()
+        {
+            stack_left_potential.Children.Clear();
+            if (actualPageNumber > 0)
+            {
+                flo_left_potential.IsOpen = true;
+                DB_CAW t = new DB_CAW();
+                List<String> list = new List<String>();
+                list = t.makeQuery(Query.getPotentialNamesFromPageNumber(actualPageNumber, this.ID));
+                foreach (var item in list)
+                {
+                    Tile tile = new Tile();
+                    tile.Title = item;
+                    tile.TiltFactor = 2;
+                    tile.Width = 150;
+                    tile.Height = 50;
+                    tile.Click += new RoutedEventHandler(Tile_Potential_Click);
+                    stack_left_potential.Children.Add(tile);
+
+                }
+            }
+        }
+
+        private void buildSiteFylout()
+        {
+            stack_right_sites.Children.Clear();
+
+        }
+
+        private void nextPage()
+        {
             if (actualPageNumber <= maxPageNumber)
             {
                 actualPageNumber++;
                 PageNumberLabel.Text = actualPageNumber.ToString();
+                if (flo_left_potential.IsOpen)
+                {
+                    buildPotentialFlyout();
+                }
             }
         }
 
-        private void Button_previous_page(object sender, RoutedEventArgs e)
+        private void previousPage()
         {
             if (actualPageNumber > 1)
             {
                 actualPageNumber--;
                 PageNumberLabel.Text = actualPageNumber.ToString();
+                if (flo_left_potential.IsOpen)
+                {
+                    buildPotentialFlyout();
+                }
             }
         }
-
-        private void Button_open_info(object sender, RoutedEventArgs e)
+        private void goToPage(int page)
         {
-            flo_right.IsOpen = true;
-        }
-
-        private void Tile_Potentiale(object sender, RoutedEventArgs e)
-        {
-            flo_left_pot.IsOpen = true;
-            DB_CAW t = new DB_CAW();
-            List<String> list = new List<String>();
-           list = t.makeQuery(Query.getPotentialNamesFromPageNumber(actualPageNumber));
-
-
-            foreach (var item in list)
-            {         
-                Tile tile = new Tile();
-                tile.Title = item;
-                tile.TiltFactor=2;
-                tile.Width = 150;
-                tile.Height = 50;
-                tile.Click += new RoutedEventHandler(Tile_Click);
-                fly_left_pot_stackpanel.Children.Add(tile);
-
-            }
-           
-        }
-        private void Tile_Click(object sender, EventArgs e)
-        {
-            Tile tile = sender as Tile;
-            DB_CAW t = new DB_CAW();
-            List<String> list = new List<String>();
-           list= t.makeQuery(Query.getPotentialPagenumbersFromPotentialNames(tile.Title));
-
-           foreach (var item in list)
-           {
-               MessageBox.Show(item);
-           }
-
+            actualPageNumber = page;
         }
     }
 }
--- bgx_caw/bgx_caw/Query.cs
+++ bgx_caw/bgx_caw/Query.cs
@@ -8,9 +8,9 @@ namespace bgx_caw
 {
     class Query
     {
-        public static String getPotentialNamesFromPageNumber(int number)
+        public static String getPotentialNamesFromPageNumber(int number, String id)
         {
-            return "SELECT dbo.tblPotential.Name FROM dbo.tblPotential INNER JOIN dbo.tblPage ON dbo.tblPotential.P_id = dbo.tblPage.P_id  INNER JOIN dbo.tblDiagramm ON dbo.tblPage.D_id = dbo.tblDiagramm.D_ID where dbo.tblPage.OriginNumber = '" + number + "'";
+            return "SELECT dbo.tblPotential.Name FROM dbo.tblPotential INNER JOIN dbo.tblPage ON dbo.tblPotential.P_id = dbo.tblPage.P_id  INNER JOIN dbo.tblDiagramm ON dbo.tblPage.D_id = dbo.tblDiagramm.D_ID where dbo.tblPage.OriginNumber = '" + number + "' AND dbo.tblDiagramm.D_ID = '"+id+"'";
         }
         public static String getPartNameFromPageNumber(int number)
         {
@@ -20,9 +20,9 @@ namespace bgx_caw
             "ON dbo.tblPage.D_id = dbo.tblDiagramm.D_ID" +
             "WHERE dbo.tblPage.OriginNumber = '" + number + "'";
         }
-        public static String getPotentialPagenumbersFromPotentialNames(String name)
+        public static String getPotentialPagenumbersFromPotentialNames(String name, String id)
         {
-            return "SELECT dbo.tblPage.OriginNumber FROM dbo.tblPotential INNER JOIN dbo.tblPage ON dbo.tblPotential.P_id = dbo.tblPage.P_id INNER JOIN dbo.tblDiagramm ON dbo.tblPage.D_id = dbo.tblDiagramm.D_ID WHERE dbo.tblPotential.Name ='" + name + "'";        
+            return "SELECT dbo.tblPage.OriginNumber FROM dbo.tblPotential INNER JOIN dbo.tblPage ON dbo.tblPotential.P_id = dbo.tblPage.P_id INNER JOIN dbo.tblDiagramm ON dbo.tblPage.D_id = dbo.tblDiagramm.D_ID WHERE dbo.tblPotential.Name ='" + name + "'AND dbo.tblDiagramm.D_ID = '" + id + "'";        
         }
     }
 }
